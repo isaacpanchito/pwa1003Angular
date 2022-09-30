bb97ebafbdb827153bae8b553e9bdc354eaac0b4
isaacpanchito <isaacemj.26@gmail.com>
2022-09-30 13:19:42 -0500

    clase del 29 de sep

---
+++ src/app/calculos/tabla/tabla.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla',
+  templateUrl: './tabla.component.html',
+  styleUrls: ['./tabla.component.css']
+})
+export class TablaComponent{
+
+  constructor() {
+    this.totalA=[];
+   }
+   total:number=0;
+   totalA:string[];  
+   res:string;
+   numero;
+
+   tabla(){
+    for (let i=1;i<=10;++i){
+      this.totalA[i]=(`${this.numero} x ${i} = ${this.numero*i}`+"\n");
+    }
+   }
+}
---
+++ src/app/cinepolis/cinepolis.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
+
+@Component({
+  selector: 'app-cinepolis',
+  templateUrl: './cinepolis.component.html',
+  styleUrls: ['./cinepolis.component.css']
+})
+export class CinepolisComponent {
+
+  constructor() { }
+  total:number=0;
+  maxBoletos:number=7;
+  error:string;
+  comprador;
+  nombre;
+  boletos;
+  tarjeta;
+
+
+
+
+
+
+  procesar(){
+    this.maxBoletos=7*this.comprador
+    if(this.boletos<=this.maxBoletos){
+      this.error=""
+      if(this.boletos<=2){
+        this.total=this.boletos*12;
+        if(this.tarjeta="si"){
+          this.total=this.total-(this.total*0.10);
+        }
+      }
+      else if(this.boletos>=3 && this.boletos<=5){
+        this.total=(this.boletos*12)-((this.boletos*12)*0.10);
+        if(this.tarjeta="si"){
+          this.total=this.total-(this.total*0.10);
+        }
+      }
+      else if(this.boletos>=6){
+        this.total=(this.boletos*12)-((this.boletos*12)*0.15);
+        if(this.tarjeta="si"){
+          this.total=this.total-(this.total*0.10);
+        }
+      }
+    }
+    else{
+      this.error="Máximo 7 boletos por comprador"
+    }
+    
+
+
+
+
+
+
+
+
+
+  }
+}
